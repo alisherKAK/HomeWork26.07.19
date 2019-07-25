38793b15fbee11c671655b9a7e067494cf925a1d
alisherKak <alisher.alish333@gmail.com>
2019-07-25 10:53:32 +0600

    Добавьте файлы проекта.

---
+++ HomeWork26_07_19/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork26_07_19
+{
+    public interface IUser //интерфейс обзервера
+    {
+        void Update(ITumbler tumbler);
+    }
+
+    public interface ITumbler //интерфейс тумблера который будет оповещать о новинках
+    {
+        string News { get; set; } //новость которая будет оповещаться
+        void Attach(IUser user); //метод подписки на рассылку
+        void Detach(IUser user); //метод отписки на рассылку
+        void Notify(); //метод оповещение
+    }
+
+    public class Tumbler : ITumbler //конкретная реализация тумблера
+    {
+        private string _news = "There is must be some news"; //новость которая будеть рассылаться
+        private List<IUser> _users = new List<IUser>(); //список клиентов которым будет отправляться рассылка
+        public string News
+        {
+            get
+            {
+                return _news;
+            }
+            set
+            {
+                _news = value;
+            }
+        }
+
+        public void Attach(IUser user)
+        {
+            _users.Add(user);
+        }
+
+        public void Detach(IUser user)
+        {
+            _users.Remove(user);
+        }
+
+        public void Notify()
+        {
+            foreach(var user in _users)
+            {
+                user.Update(this);
+            }
+        }
+    }
+
+    public class User : IUser //конкретная реализация юзера
+    {
+        public void Update(ITumbler tumbler)
+        {
+            Console.WriteLine(tumbler.News);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tumbler tumbler = new Tumbler();
+            User user = new User();
+            tumbler.Attach(user);
+            tumbler.Notify();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public interface ITravelStrategy //интерфейс стратегий
+    {
+        string Travel(List<string> places);
+    }
+
+    public class CarTravelStrategy : ITravelStrategy //конкретная реализация интерфейса ITravelStrategy
+    {
+        public string Travel(List<string> places) //конкретная реализация метода интерфейса ITravelStrategy
+        {
+            string result = "Car go:";
+            foreach(string place in places)
+            {
+                result += place + ", ";
+            }
+
+            return result;
+        }
+    }
+
+    public class BusTravelStrategy : ITravelStrategy //конкретная реализация интерфейса ITravelStrategy
+    {
+        public string Travel(List<string> places) //конкретная реализация метода интерфейса ITravelStrategy
+        {
+            string result = "Bus go:";
+            foreach (string place in places)
+            {
+                result += place + ", ";
+            }
+
+            return result;
+        }
+    }
+
+    public class MotorbykeTravelStrategy : ITravelStrategy //конкретная реализация интерфейса ITravelStrategy
+    {
+        public string Travel(List<string> places) //конкретная реализация метода интерфейса ITravelStrategy
+        {
+            string result = "Motorbyke go:";
+            foreach (string place in places)
+            {
+                result += place + ", ";
+            }
+
+            return result;
+        }
+    }
+
+    public class Trip //контекст в которм используется интерфейс ITravelStrategy
+    {
+        private ITravelStrategy _travelStrategy; //используемая стратегия
+
+        public Trip(){}
+        public Trip(ITravelStrategy travelStrategy)
+        {
+            _travelStrategy = travelStrategy;
+        }
+
+        public void SetStrategy(ITravelStrategy travelStrategy) //метод задачи стратегий
+        {
+            _travelStrategy = travelStrategy;
+        }
+
+        public void GoToTrip()
+        {
+            var places = new List<string> { "Balhash", "Almaty", "Kostanai", "Aktau" };
+            string result = _travelStrategy.Travel(places); //применение стратегий
+
+            Console.WriteLine(result);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trip trip = new Trip();
+            CarTravelStrategy carTravel = new CarTravelStrategy();
+            BusTravelStrategy busTravel = new BusTravelStrategy();
+            MotorbykeTravelStrategy motorbykeTravel = new MotorbykeTravelStrategy();
+
+            trip.SetStrategy(carTravel);
+            trip.GoToTrip();
+            trip.SetStrategy(busTravel);
+            trip.GoToTrip();
+            trip.SetStrategy(motorbykeTravel);
+            trip.GoToTrip();
+        }
+    }
+}
